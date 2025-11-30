cd26f3056e01fa25e93b31431feb9a2a8cf816a6
Yan Fonseca <165041733+YanFonsecaz@users.noreply.github.com>
2025-11-30 16:23:49 -0300

    Adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-function Home(){
-    return <h1>Papis ama muito meu amor feeder, papis nunca vai enjoar do meu amor, papis nao vive mais sem meu amor feeder </h1>
+function Home() {
+  return (
+    <h1>
+      Papis ama muito meu amor feeder, papis nunca vai enjoar do meu amor, papis
+      nao vive mais sem meu amor feeder
+    </h1>
+    console.log('Hello World!')
+  );
 }
 
 export default Home;
