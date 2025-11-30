3410013f24dbba99e96e1db9f5030817696987de
Yan Fonseca <165041733+YanFonsecaz@users.noreply.github.com>
2025-11-30 00:53:19 +0000

    testando deploy

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Isa meu amor feeder </h1>
+    return <h1>Papis ama muito meu amor feeder, papis nunca vai enjoar do meu amor, papis nao vive mais sem meu amor feeder </h1>
 }
 
 export default Home;
