5f59ec0eca38fc4e7187b099b4589c2c15950e0f
Yan Fonseca <165041733+YanFonsecaz@users.noreply.github.com>
2025-11-30 22:26:16 -0300

    instalação do jest e confirando os scripts de teste

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ function Home() {
       Papis ama muito meu amor feeder, papis nunca vai enjoar do meu amor, papis
       nao vive mais sem meu amor feeder
     </h1>
-    console.log('Hello World!')
   );
 }
 
