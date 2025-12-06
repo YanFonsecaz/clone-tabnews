845f50254f18615b272d3775ae3ed0fab7ffcdd2
Yan Fonseca <165041733+YanFonsecaz@users.noreply.github.com>
2025-12-06 16:37:05 -0300

    arquivo de status finalizado

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
-import database from "/infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: "ok" });
+  const updatedAT = new Date().toISOString();
+  const queryVersion = "Show server_version;";
+  const versionBanco = await database.query(queryVersion);
+  const queryMaxConect = await database.query("SHOW max_connections;");
+  const maxConect = queryMaxConect.rows[0].max_connections;
+
+  const dataBaseName = process.env.POSTGRES_DB;
+  const queryCurrentConect = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity Where datname = $1;",
+    values: [dataBaseName],
+  });
+  const currentConect = queryCurrentConect.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAT,
+    dependencies: {
+      version: versionBanco.rows[0].server_version,
+      max_connections: parseInt(maxConect),
+      current_connections: currentConect,
+    },
+  });
 }
 
 export default status;
