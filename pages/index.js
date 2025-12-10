603794dd72c538bd587c382dcb6a84677f80129e
Yan Fonseca <165041733+YanFonsecaz@users.noreply.github.com>
2025-12-10 11:54:09 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 function Home() {
-  return (
-    <h1>
-      Papis ama muito meu amor feeder, papis nunca vai enjoar do meu amor, papis
-      nao vive mais sem meu amor feeder
-    </h1>
-  );
+  return <h1>Teste de Deploy</h1>;
 }
 
 export default Home;
